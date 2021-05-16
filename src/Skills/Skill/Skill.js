479d8c6eed823d0b088bde6d7dfa6c34ba3d7a60
espere-papillon <marinabov007@gmail.com>
2021-05-16 23:34:20 +0300

    init: add description skills

---
+++ src/Skills/Skill/Skill.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Skill.module.css"
+
+export const Skill = (props) => {
+    return (
+        <div className={styles.skillBlock}>
+            {/*<img src={props.img} alt="Image skill"/>*/}
+            <div className={styles.img}></div>
+            <h3>{props.title}</h3>
+            <span>{props.description}</span>
+        </div>
+    )
+}
